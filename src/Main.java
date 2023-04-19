efa13e9a1056f60740f6b1dd7e9dc32ceb53a6e1
Artem <tema1997s@gmail.com>
2023-04-19 20:15:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_555;
+        int oneMilePrice = 20;
+        int mileBonus = ticketPrice/oneMilePrice;
+        System.out.println(mileBonus + " бонусных миль.");
+    }
+}
